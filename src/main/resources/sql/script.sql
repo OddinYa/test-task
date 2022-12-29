c95a98c40b978635e154f3bb961e3beb899e97b9
OddinYa <oddinserj@gmail.com>
2022-12-29 17:36:32 +0300

    add password

--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -3,7 +3,7 @@ DROP DATABASE IF EXISTS sk_example_db;
 
 DROP USER IF EXISTS sk_example_user;
 
-CREATE USER sk_example_user;
+CREATE USER sk_example_user PASSWORD '123';
 
 CREATE DATABASE sk_example_db OWNER sk_example_user;
 
